ba198466ecf3e1d5513af50164b9a66e67d80292
mg4tech <metata08@gmail.com>
2026-05-23 01:37:40 +0000

    ci/cd: correct CI test patterns to avoid e2e and empty suite errors

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: '.',
+  rootDir: 'src',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
-  collectCoverageFrom: ['src/core/**/*.ts', 'src/use-cases/**/*.ts'],
-  coverageDirectory: './coverage',
+  collectCoverageFrom: ['core/**/*.ts', 'use-cases/**/*.ts'],
+  coverageDirectory: '../coverage',
   testEnvironment: 'node',
 };
